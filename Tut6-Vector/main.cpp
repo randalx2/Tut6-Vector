9157efd69e8962d4ae53626d679924409b251968
randalx2 <pmisthry@gmail.com>
2016-04-09 23:54:28 +0200

    Created shell code

---
+++ Tut6-Vector/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cmath>
+#include <ctime>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+
+	system("PAUSE");
+	return 0;
+}
